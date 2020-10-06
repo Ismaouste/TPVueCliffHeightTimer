c55478245c009e8c3ab694cb7a8a5833b7d0e981
Ismael Rodmacq <ismael.rodmacq@gmail.com>
2020-10-06 09:32:36 +0200

    MyToolbar component added

---
+++ src/components/MyToolbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-app-bar dark color="deep-purple" clipped-left app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title @click="goHome" class="white--text">Cliff height timer</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'myToolbar',
+  data: function () {
+    return {
+      drawer: false
+    }
+  },
+  methods: {
+    goHome () {
+      this.$router.replace('/')
+    }
+  }
+}
+</script>
+
